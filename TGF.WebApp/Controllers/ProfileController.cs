a3b7774f8a4fbade0d82d2e07b6126c7f60c40ea
kacper <kacper.tarlowski20@gmail.com>
2022-01-16 23:49:41 +0100

    add web app and manage profiles

---
+++ TGF.WebApp/Controllers/ProfileController.cs
@@ -0,0 +1,199 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using TGF.WebApp.Models;
+
+namespace TGF.WebApp.Controllers
+{
+    public class ProfileController : Controller
+    {
+        public IConfiguration Configuration;
+
+        public ProfileController(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public ContentResult GetHostUrl()
+        {
+            var result = Configuration["RestApiUrl:HostUrl"];
+            return Content(result);
+        }
+
+        private string CN()
+        {
+            return ControllerContext.RouteData.Values["controller"].ToString();
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            //var tokenString = GenerateJSONWebToken();
+
+            //string _restpath = "https://localhost:5001/profile";
+            string _restpath = GetHostUrl().Content + CN();
+
+            List<ProfileVM> profilesList = new List<ProfileVM>();
+
+            using (var httpClient = new HttpClient())
+            {
+                //httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", tokenString);
+                using (var response = await httpClient.GetAsync(_restpath))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    profilesList = JsonConvert.DeserializeObject<List<ProfileVM>>(apiResponse);
+                }
+            }
+
+            return View(profilesList);
+        }
+
+        //[Authorize]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //[Authorize]
+        [HttpPost]
+        public async Task<IActionResult> Create(ProfileVM p)
+        {
+           // var tokenString = GenerateJSONWebToken();
+            string _restpath = GetHostUrl().Content + CN();
+
+            try
+            {
+                using (var httpClient = new HttpClient())
+                {
+                    string jsonString = System.Text.Json.JsonSerializer.Serialize(p);
+                    var content = new StringContent(jsonString, Encoding.UTF8, "application/json");
+                    //httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", tokenString);
+
+                    using var response = await httpClient.PostAsync(_restpath, content);
+                }
+            }
+            catch (Exception e)
+            {
+                return View(e);
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        //[Authorize]
+        public async Task<IActionResult> Edit(int id)
+        {
+            //var tokenString = GenerateJSONWebToken();
+            string _restpath = GetHostUrl().Content + CN();
+
+            ProfileVM p = new ProfileVM();
+
+            try
+            {
+                using (var httpClient = new HttpClient())
+                {
+                    //httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", tokenString);
+                    using (var response = await httpClient.GetAsync($"{_restpath}/{id}"))
+                    {
+                        string apiResponse = await response.Content.ReadAsStringAsync();
+                        p = JsonConvert.DeserializeObject<ProfileVM>(apiResponse);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                return View(e);
+            }
+
+            return View(p);
+        }
+
+        //[Authorize]
+        [HttpPost]
+        public async Task<IActionResult> Edit(ProfileVM p)
+        {
+            //var tokenString = GenerateJSONWebToken();
+            string _restpath = GetHostUrl().Content + CN();
+
+            ProfileVM pResult = new ProfileVM();
+
+            try
+            {
+                using (var httpClient = new HttpClient())
+                {
+                    string jsonString = System.Text.Json.JsonSerializer.Serialize(p);
+                    var content = new StringContent(jsonString, Encoding.UTF8, "application/json");
+                    //httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", tokenString);
+
+                    using (var response = await httpClient.PutAsync($"{ _restpath}/{p.Id}", content))
+                    {
+                        string apiResponse = await response.Content.ReadAsStringAsync();
+                        pResult = JsonConvert.DeserializeObject<ProfileVM>(apiResponse);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                return View(e);
+            }
+
+            return View(pResult);
+        }
+
+        //[Authorize]
+        public async Task<IActionResult> Delete(int id)
+        {
+            //var tokenString = GenerateJSONWebToken();
+            string _restpath = GetHostUrl().Content + CN();
+
+            ProfileVM p = new ProfileVM();
+
+            try
+            {
+                using (var httpClient = new HttpClient())
+                {
+                    //httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", tokenString);
+                    using (var response = await httpClient.GetAsync($"{ _restpath}/{id}"))
+                    {
+                        string apiResponse = await response.Content.ReadAsStringAsync();
+                        p = JsonConvert.DeserializeObject<ProfileVM>(apiResponse);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                return View(e);
+            }
+
+            return View(p);
+        }
+
+        //[Authorize]
+        [HttpPost]
+        public async Task<IActionResult> Delete(ProfileVM p)
+        {
+            //var tokenString = GenerateJSONWebToken();
+            string _restpath = GetHostUrl().Content + CN();
+
+            try
+            {
+                using (var httpClient = new HttpClient())
+                {
+                    //httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", tokenString);
+                    using var response = await httpClient.DeleteAsync($"{ _restpath}/{p.Id}");
+                }
+            }
+            catch (Exception e)
+            {
+                return View(e);
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ TGF.WebApp/Models/ProfileVM.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TGF.WebApp.Models
+{
+    public class ProfileVM
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime LastSeen { get; set; }
+
+        // public ICollection<Character> Characters { get; set; }
+        // public ICollection<Post> Posts { get; set; }
+    }
+}
